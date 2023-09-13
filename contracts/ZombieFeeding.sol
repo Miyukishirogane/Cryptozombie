db95b2fe6933c4194a05ff31b53ded465be95ccb
Miyukishirogane <97428594+Miyukishirogane@users.noreply.github.com>
2023-09-13 13:31:04 +0700

    lesson 3

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -15,20 +15,31 @@ contract KittyInterface{
     uint256 genes
 );
 }
-
 contract ZombieFeeding is ZombieFactory {
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    //address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; remove
     //khởi tạo contract kitty bằng địa chỉ trên
-    KittyInterface kittyContract = KittyInterface(ckAddress);
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    //KittyInterface kittyContract = KittyInterface(ckAddress);
+    KittyInterface kittyContract;// chi dung de khai bao
+  function setKittyContractAddress( address _address) external onlyOwner { // khong ai ben ngoai thay doi duoc kittyContract
+    kittyContract = KittyInterface(_address);
+  }
+  function _triggerCooldown(Zombie storage _zombie) internal {
+      _zombie.readyTime = uint32(now + cooldownTime);
+  }
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+        return(_zombie.readyTime <= now);
+  }
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal { // thay tu public sang
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    require (_isReady(myZombie)); // kiem tra _isReady va pass ham myZombie 
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
      if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
   }
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
